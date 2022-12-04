e2ddaf05740096c7c4af39ad6abee7c53b8dca9d
ting-prado <ethel.prado1998@gmail.com>
2022-12-04 19:26:56 +0800

    changed test1 into a good test name

--- test/GradeBook.Tests/BookTests.cs
+++ test/GradeBook.Tests/BookTests.cs
@@ -3,7 +3,7 @@ namespace GradeBook.Tests;
 public class BookTests
 {
     [Fact]
-    public void Test1()
+    public void BookCalculatesStatistics()
     {
         // arrange
         var book = new Book("");
