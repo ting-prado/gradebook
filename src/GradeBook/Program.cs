bd97f820191fcf0f296a375dc8d4cd47abda925a
ting-prado <ethel.prado1998@gmail.com>
2022-12-06 08:29:32 +0800

    allow user to input grade on runtime

--- src/GradeBook/Program.cs
+++ src/GradeBook/Program.cs
@@ -8,14 +8,25 @@ namespace GradeBook
       static void Main(string[] args)
       {
         var book = new Book("Sample book");
-        book.AddGrade(89.1);
-        book.AddGrade(90.5);
-        book.AddGrade(77.5);
+
+        while (true)
+        {
+          Console.WriteLine("Enter a grade or press 'Q' to quit.");
+          var input = Console.ReadLine();
+          if(input == "q" || input == "q")
+          {
+            break;
+          } else {
+            var grade = double.Parse(input);
+            book.AddGrade(grade);
+          }
+        }
 
         var stats = book.GetStatistics();
         Console.WriteLine($"The lowest grade is {stats.Low}.");
         Console.WriteLine($"The highest grade is {stats.High}.");
         Console.WriteLine($"The average of grades is {stats.Average:N2}.");
+        Console.WriteLine($"The letter grade is {stats.Letter}");
       }
     }
 }
